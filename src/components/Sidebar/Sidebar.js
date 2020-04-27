eb419e1c102d2a0765e7a735bba01c0560071bf9
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-04-27 14:25:10 -0400

    Sidebar structure complete. Added Photo

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,12 +5,18 @@ export default function Sidebar(props) {
 
   return (
     <section className="sidebar">
-      <img className="mainPic" src="/images/profilePic.jpg"></img>
+      <img className="mainPic" src="/images/profilePicSquare.jpg"></img>
       <div className="sidebarDescription">
         <h3>Sheldon Malboeuf</h3>
         <p><a href="mailto:smalboeuf01@gmail.com?Subject=Inquiry" target="_top">smalboeuf01@gmail.com</a></p>
       </div>
-      <div className="social"></div>
+      <div className="navbar">
+        <a className="navButtons">About</a>
+        <a className="navButtons">Projects</a>
+        <a className="navButtons">Skills</a>
+        <a className="navButtons">Resume</a>
+      </div>
+      <div className="social">github, linkedIn, </div>
     </section>
   );
 
