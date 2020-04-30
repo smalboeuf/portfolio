8bc114a77aa67c508fd5758a516fffe352b45202
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-04-30 16:54:06 -0400

    Fix formatting for Web dev projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,18 +2,13 @@ import React from "react";
 import './Project.scss';
 
 
-
-
-
-
-
 export default function Project(props) {
 
   return (
     <div className="project">
+      <h4 className="projectTitle">{props.project.title}</h4>
       <img className="projectImage" src={props.project.image}></img>
       <div className="projectInfo">
-        <h4 className="projectTitle">{props.project.title}</h4>
         <p className="projectDescription">{props.project.description}</p>
         <p>Stack: {props.project.stack}</p>
         <a href={props.project.github}>Github</a>
