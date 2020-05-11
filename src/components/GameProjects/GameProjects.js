6bc03cdd557e7a615fbc384e1b85362010279a47
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-05-11 13:42:27 -0400

    Under construction stylings

--- src/components/GameProjects/GameProjects.js
+++ src/components/GameProjects/GameProjects.js
@@ -6,7 +6,13 @@ export default function GameProjects(props) {
   const allProjects =
     projectData &&
     projectData.map((project, index) => {
-      return <Project key={index} project={project} />;
+      return (
+        <Project
+          constructed={project.constructed}
+          key={index}
+          project={project}
+        />
+      );
     });
 
   return (
@@ -25,5 +31,15 @@ const projectData = [
     image: "/images/projects/FrogInTheStarPajamas.png",
     github: "https://imgur.com/a/M3l1xFh",
     stack: "Built in Unity (C#)",
+    constructed: true,
+  },
+  {
+    title: "Old Folks Tavern",
+    description:
+      "Frog in the Star Pajamas is a 2D platformer where you play as a frog who wants to find his pajamas so he can finally rest.",
+    image: "/images/projects/FrogInTheStarPajamas.png",
+    github: "https://imgur.com/a/M3l1xFh",
+    stack: "Built in Unity (C#)",
+    constructed: false,
   },
 ];
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./Project.scss";
+import classnames from "classnames";
 
 export default function Project(props) {
+  const imageClass = props.constructed
+    ? "projectImage"
+    : "projectImage underConstruction";
+
+  const underConstruction = props.constructed ? null : (
+    <h4 className="underConstruction">Under Construction</h4>
+  );
+
   return (
     <div className="project">
       <h4 className="projectTitle">{props.project.title}</h4>
-      <img
-        alt="Project"
-        className="projectImage"
-        src={props.project.image}
-      ></img>
+      <div className="imageContainer">
+        <img
+          alt="Project"
+          className={imageClass}
+          src={props.project.image}
+        ></img>
+        {underConstruction}
+      </div>
       <div className="projectInfo">
         <p className="projectDescription">{props.project.description}</p>
         <p>Stack: {props.project.stack}</p>
